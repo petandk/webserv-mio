466fbafe0254db2be7e31298ec583b15d00e90e7
Raúl Manzanas Trillo <rmanzana@student.42barcelona.com>
2026-05-01 14:46:57 +0200

    fcleaneado

--- obj/main.d
+++
@@ -1,7 +0,0 @@
-obj/main.o: src/main.cpp src/../inc/parser/ConfigParser.hpp \
-  inc/parser/ServerConfig.hpp inc/parser/LocationConfig.hpp \
-  inc/parser/ConfigParser.tpp inc/parser/../utils/Utils.hpp \
-  inc/parser/ConfigParser.hpp src/../inc/utils/Debug.hpp \
-  inc/utils/../parser/ConfigParser.hpp src/../inc/server/Server.hpp \
-  inc/server/../parser/ServerConfig.hpp \
-  inc/server/../parser/ConfigParser.hpp
--- obj/server/Server.d
+++
@@ -1,5 +0,0 @@
-obj/server/Server.o: src/server/Server.cpp inc/../inc/server/Server.hpp \
-  inc/server/../parser/ServerConfig.hpp inc/parser/LocationConfig.hpp \
-  inc/server/../parser/ConfigParser.hpp inc/parser/ServerConfig.hpp \
-  inc/parser/ConfigParser.tpp inc/parser/../utils/Utils.hpp \
-  inc/parser/ConfigParser.hpp
